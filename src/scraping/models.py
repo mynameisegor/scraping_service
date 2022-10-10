87368f2f50ccf89f2ad30fca639878d3b084ce46
Егор <egor.world.91@gmail.com>
2022-10-10 16:23:07 +0500

    3rd commit(добавил еще одну таблицу в бд с языками программирования, также создал файл utils для транскрипции на англ язык для slug)

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -3,12 +3,24 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название города')
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name='Название города', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Название города'
         verbose_name_plural = 'Названия городов'
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
 
     def __str__(self):
         return self.name
