f13e1acb6f96484b6e3cd2e7b98d8e3c661104c6
Егор <egor.world.91@gmail.com>
2022-10-10 16:35:44 +0500

    3rd+ commit(написал функция в models.py для перевода русских букв в англ)

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .utils import from_cyrillic_to_eng
 
 
 # Create your models here.
@@ -13,6 +14,11 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
 
 class Language(models.Model):
     name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
@@ -24,3 +30,9 @@ class Language(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
