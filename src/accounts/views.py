f731b4c75b026ea5c455da5dabc73843fdd9ff4f
Егор <egor.world.91@gmail.com>
2022-10-26 18:26:11 +0500

    16th commit(добавил удаление пользователей, зименение настроек для пользователя, а так же системные уведомления при совершении действий пользователем)

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from accounts.forms import UserLoginForm, UserRegistrationForm
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
+from django.contrib import messages
+
+User = get_user_model()
 
 
 # Create your views here.
@@ -27,6 +30,37 @@ def register_view(request):
         new_user = form.save(commit=False)
         new_user.set_password(form.cleaned_data['password'])
         new_user.save()
+        messages.success(request, 'Пользователь добавлен в систему.')
         return render(request, 'accounts/register_done.html', {'new_user': new_user})
     return render(request, 'accounts/register.html', {'form': form})
 
+
+def update_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            form = UserUpdateForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.send_email = data['send_email']
+                user.save()
+                messages.success(request, 'Данные сохранены')
+                return redirect('accounts:update')
+
+        form = UserUpdateForm(initial={'city': user.city, 'language': user.language,
+                                       'send_email': user.send_email})
+        return render(request, 'accounts/update.html', {'form': form})
+    else:
+        return redirect('accounts:login')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            qs = User.objects.get(pk=user.pk)
+            qs.delete()
+        messages.error(request, 'Пользователь удален :(')
+    return redirect('home')
